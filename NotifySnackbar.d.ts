12881281c52095f2eb28695e9ebe765a2ad16dd6
Shridhar Sharma <shridharkaushik29@gmail.com>
2021-09-15 10:11:35 +0530

    Added button content in NotifySnackbar props.

--- NotifySnackbar.d.ts
+++ NotifySnackbar.d.ts
@@ -2,6 +2,7 @@ import { SnackbarProps } from "@material-ui/core/Snackbar";
 import { SnackbarContentProps } from "@material-ui/core/SnackbarContent";
 interface NotifySnackbarProps extends Partial<SnackbarProps> {
     contentProps?: SnackbarContentProps;
+    closeText?: any;
     open?: boolean;
 }
 export default function NotifySnackbar(props: NotifySnackbarProps): JSX.Element;
